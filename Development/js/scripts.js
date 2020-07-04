a4e52793b0ac6b8c59291d7584c20bdac16f67e3
83-batoolalqallaf <66445801+83-batoolalqallaf@users.noreply.github.com>
2020-07-04 20:24:00 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,26 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
-
+function greet(name) {
+ let massage = 'hello';
+ if (name) {
+     massage += name ;
+     console.log(massage);
+ }else {
+     console.log(message);
+ }
+}
+// great();
+greet("batool");
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+    if (n % 2 == 1){
+        return true ; }
+        else {return false }
+}
+ console.log (isOdd(5));
+ console.log (isOdd(2));
